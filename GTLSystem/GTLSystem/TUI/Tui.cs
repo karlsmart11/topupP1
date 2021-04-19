8a61d17d2340f1143758f953a4c0a8bbb54db6fa
Mark Lauridsen <the.mark.lauridsen@gmail.com>
2021-04-19 14:56:37 +0200

    tui

--- GTLSystem/GTLSystem/TUI/Tui.cs
+++ GTLSystem/GTLSystem/TUI/Tui.cs
@@ -33,8 +33,32 @@ namespace GTLSystem.TUI
         private static void MainMenu()
         {
             Console.WriteLine("Georgia Tech Library Reservation system Inc.");
-            Console.WriteLine("Option 1: ");
-            Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("                Main menu");
+            Console.WriteLine("");
+            Console.WriteLine("1: Create Loan");
+
+            string input = Console.ReadLine().ToLower();
+            if(input.Equals("exit"))
+            {
+                return;
+            }
+
+            try
+            {                
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1: return;
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Menu Entry");
+                MainMenu();
+            }            
         }
     }
 }
