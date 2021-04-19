23b5e493bc48323b642a809c8f2d8c24c4577cf4
Mark Lauridsen <the.mark.lauridsen@gmail.com>
2021-04-19 12:25:00 +0200

    added project

---
+++ GTLSystem/GTLSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+
+namespace GTLSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = @"Server=localhost\SQLEXPRESS;Database=GTL;Trusted_Connection=True;";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+
+            var version = con.ExecuteScalar<string>("SELECT @@VERSION");
+
+            Console.WriteLine(version);
+        }
+    }
+}
